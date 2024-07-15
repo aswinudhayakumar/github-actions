f3dfa0eeed50a3e3928ffb8954bd7d910967aa0b
Aswin Udhayakumar <aswiniu03@gmail.com>
2024-07-15 23:03:37 +0530

    Add unit test

---
+++ main_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestReverseString(t *testing.T) {
+	data := "data"
+	expected := "atad"
+
+	actual := reverseString(data)
+
+	assert.Equal(t, expected, actual)
+}
